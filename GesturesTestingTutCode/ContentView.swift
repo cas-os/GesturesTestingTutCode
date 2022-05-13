d447252d39e1ca79eb10e62bf1412b391330e7f1
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-05-13 11:17:43 +0200

    /.1

--- GesturesTestingTutCode/ContentView.swift
+++ GesturesTestingTutCode/ContentView.swift
@@ -9,8 +9,79 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        //DragGesture_001()
+        DragGesture_002()
+    }
+}
+
+
+struct DragGesture_002: View {
+    @GestureState private var isMoving: Bool = false
+    @State private var rectPosition = CGPoint(x: 100, y: 100)
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Circle()
+                    .fill(isMoving ? Color.red : Color.green)
+                    .frame(width: 50, height: 50)
+                Spacer()
+                
+            }
+            
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.blue)
+                .frame(width: 100, height: 100)
+                .position(rectPosition)
+                .gesture(
+                    DragGesture()
+                        .onChanged({ value in
+                        self.rectPosition = value.location
+                        })
+                    // .updating binding and body passed params to check and explain
+                        .updating($isMoving, body: { (value, state, transaction) in
+                            state = true
+                        })
+                )
+        }
+    }
+}
+
+struct DragGesture_001: View {
+    @State private var rectPosition = CGPoint(x: UIScreen.main.bounds.width / 2, y: 100)
+    
+    var body: some View {
+        ZStack {
+            ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.blue)
+                .frame(width: 100, height: 100)
+                .position(rectPosition) // position of the *center* of view
+                .gesture(DragGesture()
+                            .onChanged({ value in
+    //                            print(".onChanged :: value :: \(value)")
+    //                            print(".onChanged :: value.predictedEndLocation :: \(value.predictedEndLocation)")
+                                self.rectPosition = value.location
+                                    }
+                            )
+                            .onEnded({ value in
+    //                print(".onEnded :: value.location ::  \(value.location)")
+    //                print(".onEnded :: value ::  \(value)")
+    //                print(".onEnded :: value.predictedEndLocation :: \(value.predictedEndLocation)")
+                                    }
+                            )
+                            
+                )
+            
+            VStack {
+                Text("location x: \(self.rectPosition.x)")
+                Text("location y: \(self.rectPosition.y)")
+            }
+            }
+            
+
+        }
     }
 }
 
