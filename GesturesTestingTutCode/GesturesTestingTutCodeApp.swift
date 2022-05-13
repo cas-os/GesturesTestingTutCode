17934a467f87b8514e2483c1b8540c1ec55628ac
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-05-13 07:45:39 +0200

    Initial Commit

---
+++ GesturesTestingTutCode/GesturesTestingTutCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GesturesTestingTutCodeApp.swift
+//  GesturesTestingTutCode
+//
+//  Created by HAL-9001 on 13/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct GesturesTestingTutCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
