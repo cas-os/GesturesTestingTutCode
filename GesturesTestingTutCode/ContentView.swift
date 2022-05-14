ead9b195db58c3da2159a2816d59b1e7ec7d5f9d
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-05-14 22:05:00 +0200

    /.2

--- GesturesTestingTutCode/ContentView.swift
+++ GesturesTestingTutCode/ContentView.swift
@@ -9,8 +9,94 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        //DragGesture_001()
-        DragGesture_002()
+        DragGesture_004()
+        
+    }
+}
+
+
+struct DragGesture_004: View {
+    @State private var siderPosition: CGFloat = 50 - UIScreen.main.bounds.width
+    @GestureState private var offset = CGSize.zero
+    
+    
+    
+    var body: some View {
+        GeometryReader { geoProxy in
+            VStack {
+                Text("test_001")
+                    .font(.largeTitle)
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(systemName: "line.horizontal.3")
+//                        .font(.largeTitle)
+//                        .frame(width: 100, height: 100)
+                        .rotationEffect(.degrees(90))
+//                        .foregroundColor(Color.orange)
+//                        .background(Color.blue)
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                .frame(height: 100)
+                .padding()
+                .background {
+                    ZStack {
+                        Rectangle().fill(Color.orange).offset(x: -100)
+//                            .fill(Color.orange)
+//                            .offset(x: -100)
+                        Capsule().fill(Color.orange)
+                    }
+                }
+//                Spacer()
+            }
+        }
+//        RoundedRectangle(cornerRadius: 20)
+//            .fill(Color.orange)
+//            .frame(width: 100, height: 100)
+        
+        
+        
+    }
+}
+
+struct DragGesture_003: View {
+    @State private var rectPosition = CGPoint(x: 100, y: 100)
+    @State private var overlapping = false
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 50)
+                .stroke(overlapping ? Color.red : Color.clear, style: StrokeStyle(lineWidth: overlapping ? 40 : 0))
+            .edgesIgnoringSafeArea(.all)
+            
+            RoundedRectangle(cornerRadius: 20)
+            //    .frame(width: 100, height: 100)
+                .strokeBorder(Color.orange, style: StrokeStyle(lineWidth: overlapping ? 2 : 0))
+                .background(overlapping ? Color.clear : Color.orange)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .frame(width: 100, height: 100)
+                .position(rectPosition)
+                .gesture(
+                    DragGesture()
+                        .onChanged({ value in
+                            self.rectPosition = value.location
+                        })
+                        .onEnded({ value in
+                            // if position is too far beyond 'border' it is not possible to move rectangle back. why ?
+                            if value.location.x < UIScreen.main.bounds.minX + 50 ||
+                                value.location.x > UIScreen.main.bounds.maxX - 50 ||
+                                value.location.y < UIScreen.main.bounds.minY + 50 ||
+                                value.location.y > UIScreen.main.bounds.maxY - 50 {
+                                self.overlapping = true
+                            } else {
+                                self.overlapping = false
+                            }
+                        })
+                )
+        }
+
+        
     }
 }
 
